49a1fdc129052c60c439250d522d7b25a767cf1f
ushashir <laleoict@gmail.com>
2024-03-09 13:42:04 +0000

    issue-2 eslint and prettier successfully added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
       colors: {
-        primary: '#36827F', 
-        secondary: '#F9DB6D', 
-        accent: '#464D77' 
-      }
+        primary: '#36827F',
+        secondary: '#F9DB6D',
+        accent: '#464D77',
+      },
     },
     extend: {},
   },
   plugins: [],
-}
-
+};
