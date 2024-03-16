840a0dd6aff8d103e7bccd11a757fa8fe0213d5f
ushashir <laleoict@gmail.com>
2024-03-16 17:53:01 +0000

    triplist and trip component created

--- src/components/common/NavBar/index.jsx
+++ src/components/common/NavBar/index.jsx
@@ -27,7 +27,7 @@ export default function NavBar() {
           <div className="flex lg:flex-1">
             <a href="#" className="-m-1.5 p-1.5">
               <span className="sr-only">
-                Your Company
+                gomonji
               </span>
               <img
                 className="h-8 w-auto"
--- src/components/features/TripDetails/index.jsx
+++ src/components/features/TripDetails/index.jsx
@@ -1,1 +1,73 @@
-// create component here
+import React from "react";
+import PropTypes from "prop-types";
+
+const TripList = ({ trips }) => {
+  return (
+    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+      {trips.map((trip, index) => (
+        <TripDetails key={index} {...trip} />
+      ))}
+    </div>
+  );
+};
+
+TripList.propTypes = {
+  trips: PropTypes.arrayOf(
+    PropTypes.shape({
+      picture: PropTypes.string.isRequired,
+      title: PropTypes.string.isRequired,
+      startDate: PropTypes.string.isRequired,
+      endDate: PropTypes.string.isRequired,
+      noOfPeople: PropTypes.number.isRequired,
+      amount: PropTypes.number.isRequired,
+      bookButton: PropTypes.node.isRequired,
+    })
+  ).isRequired,
+};
+
+const TripDetails = ({
+  picture,
+  title,
+  startDate,
+  endDate,
+  noOfPeople,
+  amount,
+  bookButton,
+}) => {
+  return (
+    <div className="bg-white p-4 rounded-lg shadow-md">
+      <img
+        src={picture}
+        alt={title}
+        className="w-full h-auto mb-4"
+      />
+      <h2 className="text-lg font-semibold mb-2">
+        {title}
+      </h2>
+      <p className="text-gray">
+        {startDate} - {endDate}
+      </p>
+      <p className="text-gray mb-2">
+        Number of people: {noOfPeople}
+      </p>
+      <div className="flex justify-between">
+        <p className="text-gray mb-2">
+          Amount: ${amount}
+        </p>
+        <div className="">{bookButton}</div>
+      </div>
+    </div>
+  );
+};
+
+TripDetails.propTypes = {
+  picture: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  startDate: PropTypes.string.isRequired,
+  endDate: PropTypes.string.isRequired,
+  noOfPeople: PropTypes.number.isRequired,
+  amount: PropTypes.number.isRequired,
+  bookButton: PropTypes.node.isRequired,
+};
+
+export { TripList, TripDetails };
