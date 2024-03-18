5033b5546b990f0dc5a63b7a3f1a08cbd1bca6a8
ushashir <laleoict@gmail.com>
2024-03-18 19:01:24 +0000

    added homepage

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,4 +1,3 @@
-import PropTypes from "prop-types";
 import trip from "../../assets/images/trips-images/Rectangle 11.png";
 import trip1 from "../../assets/images/trips-images/Rectangle 9 (1).png";
 import trip2 from "../../assets/images/trips-images/Rectangle 9.png";
@@ -6,6 +5,7 @@ import trip3 from "../../assets/images/trips-images/Rectangle 9 (2).png";
 import { TripList } from "../../components/features/TripDetails";
 import NavBar from "../../components/common/NavBar";
 import Footer from "../../components/common/Footer";
+import PropTypes from "prop-types";
 
 export default function HomePage() {
   return (
@@ -14,7 +14,8 @@ export default function HomePage() {
         <NavBar />
         <HeroSection />
       </div>
-      <SampleComponent />
+      <PopularDestinationsSection />
+      <AllTripsSection />
       <Footer />
     </>
   );
@@ -42,37 +43,47 @@ const HeroSection = () => {
   );
 };
 
-const TeamMember = ({
-  avatar,
-  title,
-  description,
-}) => {
+const PopularDestinationsSection = () => {
+  const popularDestinationsData = [
+    {
+      id: 1,
+      picture: trip,
+      country: "Lagos",
+    },
+    {
+      id: 2,
+      picture: trip1,
+      country: "Lome",
+    },
+    {
+      id: 3,
+      picture: trip2,
+      country: "Lusaka",
+    },
+    {
+      id: 4,
+      picture: trip3,
+      country: "Accra",
+    },
+  ];
   return (
-    <div className="flex flex-col sm:flex-row items-center border border-secondary-100 mb-6 rounded-lg">
-      <img
-        src={avatar}
-        alt={`Avatar of ${title}`}
-        className="w-12 h-12 rounded-full sm:mr-8"
-      />
-      <div className="mt-3 sm:mt-0">
-        <h3 className="text-base sm:text-lg lg:text-xl font-semibold">
-          {title}
-        </h3>
-        <p className="text-sm sm:text-base text-gray-600">
-          {description}
-        </p>
+    <>
+      <div className="text-center mt-10 mb-10">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 mt-6"></div>
       </div>
-    </div>
+
+      <div className="container mx-auto py-8">
+        <h1 className="text-3xl font-bold mb-6">
+          Popular Destinations
+        </h1>
+        <PolularTrips
+          trips={popularDestinationsData}
+        />
+      </div>
+    </>
   );
 };
-
-TeamMember.propTypes = {
-  avatar: PropTypes.string.isRequired,
-  title: PropTypes.string.isRequired,
-  description: PropTypes.string.isRequired,
-};
-
-const SampleComponent = () => {
+const AllTripsSection = () => {
   const tripsData = [
     {
       id: 1,
@@ -82,25 +93,17 @@ const SampleComponent = () => {
       endDate: "2024-06-20",
       noOfPeople: 4,
       amount: 1200,
-      bookButton: (
-        <button className="bg-blue text-white px-4 py-2 rounded">
-          Book Now
-        </button>
-      ),
+      bookButton: <BookNowButton />,
     },
     {
       id: 2,
       picture: trip1,
-      title: "Mountain Hiking Expedition",
+      title: "Mountain Hiking",
       startDate: "2024-07-10",
       endDate: "2024-07-15",
       noOfPeople: 6,
       amount: 1800,
-      bookButton: (
-        <button className="bg-blue text-white px-4 py-2 rounded">
-          Book Now
-        </button>
-      ),
+      bookButton: <BookNowButton />,
     },
     {
       id: 3,
@@ -110,11 +113,47 @@ const SampleComponent = () => {
       endDate: "2024-06-20",
       noOfPeople: 4,
       amount: 1200,
-      bookButton: (
-        <button className="bg-blue text-white px-4 py-2 rounded">
-          Book Now
-        </button>
-      ),
+      bookButton: <BookNowButton />,
+    },
+    {
+      id: 4,
+      picture: trip3,
+      title: "Destiny Land",
+      startDate: "2024-07-10",
+      endDate: "2024-07-15",
+      noOfPeople: 6,
+      amount: 1800,
+      bookButton: <BookNowButton />,
+    },
+    {
+      id: 1,
+      picture: trip,
+      title: "Trip to Beach Paradise",
+      startDate: "2024-06-15",
+      endDate: "2024-06-20",
+      noOfPeople: 4,
+      amount: 1200,
+      bookButton: <BookNowButton />,
+    },
+    {
+      id: 2,
+      picture: trip1,
+      title: "Mountain Hiking Expedition",
+      startDate: "2024-07-10",
+      endDate: "2024-07-15",
+      noOfPeople: 6,
+      amount: 1800,
+      bookButton: <BookNowButton />,
+    },
+    {
+      id: 3,
+      picture: trip2,
+      title: "Trip to Beach Paradise",
+      startDate: "2024-06-15",
+      endDate: "2024-06-20",
+      noOfPeople: 4,
+      amount: 1200,
+      bookButton: <BookNowButton />,
     },
     {
       id: 4,
@@ -124,13 +163,8 @@ const SampleComponent = () => {
       endDate: "2024-07-15",
       noOfPeople: 6,
       amount: 1800,
-      bookButton: (
-        <button className="bg-blue text-white px-4 py-2 rounded">
-          Book Now
-        </button>
-      ),
+      bookButton: <BookNowButton />,
     },
-    // Add more trip data objects
   ];
   return (
     <>
@@ -145,5 +179,52 @@ const SampleComponent = () => {
         <TripList trips={tripsData} />
       </div>
     </>
+  );
+};
+
+const PolularTrips = ({ trips }) => {
+  return (
+    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+      {trips.map((trip, index) => (
+        <TripDetails key={index} {...trip} />
+      ))}
+    </div>
+  );
+};
+
+PolularTrips.propTypes = {
+  trips: PropTypes.arrayOf(
+    PropTypes.shape({
+      picture: PropTypes.string.isRequired,
+      country: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+};
+
+const TripDetails = ({ picture, country }) => {
+  return (
+    <div className="bg-white rounded-lg shadow-md">
+      <img
+        src={picture}
+        alt={country}
+        className="w-full h-auto mb-4"
+      />
+      <h2 className="text-lg font-semibold mb-2">
+        {country}
+      </h2>
+    </div>
+  );
+};
+
+TripDetails.propTypes = {
+  picture: PropTypes.string.isRequired,
+  country: PropTypes.string.isRequired,
+};
+
+const BookNowButton = () => {
+  return (
+    <button className="bg-primary text-white px-4 py-2 m-4 rounded">
+      Book Now
+    </button>
   );
 };
